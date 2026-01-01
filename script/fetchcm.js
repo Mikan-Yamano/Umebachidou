dc324b4e239076aa49cbc42f93bde87f3b263aa9
Mikan_Yamano <anne_sh2003@outlook.com>
2025-12-31 22:48:57 -0800

    cmmod

--- script/fetchcm.js
+++ script/fetchcm.js
@@ -1,6 +1,12 @@
-fetch("https://mikan-yamano.workers.dev?page=https://mikan-yamano.github.io/Umebachidou/comment/Yumeno-Dogura/01.html")
-  .then(r => r.json())
-  .then(d => {
-    document.getElementById("comment-count").textContent =
-      d.totalComments;
-  });
+// fetch("https://mikan-yamano.workers.dev?page=https://mikan-yamano.github.io/Umebachidou/comment/Yumeno-Dogura/01.html")
+//   .then(r => r.json())
+//   .then(d => {
+//     document.getElementById("comment-count").textContent =
+//       d.totalComments;
+//   });
+
+<script>
+  const el = document.querySelector(".comment-count");
+  console.log("element =", el);
+  if (el) el.textContent = "FOUND";
+</script>
