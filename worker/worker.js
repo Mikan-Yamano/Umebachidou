4a280464055f69c52b07c0ea73d200a0e5cbb7aa
Mikan_Yamano <anne_sh2003@outlook.com>
2025-12-31 21:09:29 -0800

    worker

---
+++ worker/worker.js
@@ -0,0 +1,13 @@
+export default {
+  async fetch(request, env) {
+    return new Response(
+      JSON.stringify({ status: 'ok' }),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        }
+      }
+    );
+  }
+};
