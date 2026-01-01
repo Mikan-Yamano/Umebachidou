f0f646b7cb0ffb48c80fdea0b70f4b46a171ac0a
Mikan_Yamano <anne_sh2003@outlook.com>
2025-12-31 22:34:24 -0800

    comsumtest

---
+++ script/fetchcm.js
@@ -0,0 +1,6 @@
+fetch("https://mikan-yamano.workers.dev?https://mikan-yamano.github.io/Umebachidou/comment/Yumeno-Dogura/01.html")
+  .then(r => r.json())
+  .then(d => {
+    document.getElementById("comment-count").textContent =
+      d.totalComments;
+  });
