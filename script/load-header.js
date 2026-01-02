a411cb1c55a797044a25d79b60734adc9e5df523
Mikan_Yamano <anne_sh2003@outlook.com>
2026-01-02 13:35:46 -0800

    headermod

--- script/load-header.js
+++ script/load-header.js
@@ -1,11 +1,11 @@
 class SiteHeader extends HTMLElement {
     connectedCallback() {
 	this.innerHTML = `
-      <base href="/Umebachidou">
+      <base href="/Umebachidou/">
       <header class="site-header">
       <nav class="nav-buttons">
-	<button><a href="/index.html"/>玄関</a></button>
-<button><a href="/bookshelf/Yumeno-Dogura.html"/>本棚</a></button>
+	<button><a href="index.html"/>玄関</a></button>
+<button><a href="bookshelf/Yumeno-Dogura.html"/>本棚</a></button>
 <button>御礼</button>
 <button>便り</button>
 </nav>
